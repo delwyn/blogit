165ca8cb765f2e151a481b7c4963ac60e75cff34
Delwyn de Villiers <delwyn.d@gmail.com>
2011-02-11 10:06:35 +0000

    Initial commit

---
+++ lib/blogit.rb
@@ -0,0 +1,2 @@
+module Blogit
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '882a9c845b8b25ec240d17ac8fc48eef63762bcfa33f991006f96856319980a25f8d45f1e9686799114544a1fbaf4a2d8c7e185b0c50331707624ef2eedd7ac0'
